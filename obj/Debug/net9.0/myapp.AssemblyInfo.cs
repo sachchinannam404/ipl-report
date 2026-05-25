a579279ba0694f9f135f4eb9160eb430e0178213
sachchinannam <sachchinannam@gmail.com>
2026-05-25 16:00:38 +0000

    commit

--- obj/Debug/net9.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9217f54a959cfb91532d1f3fe363b80fa10979ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba49b7821a99fd26bf5f2fc0d620731489692923")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
