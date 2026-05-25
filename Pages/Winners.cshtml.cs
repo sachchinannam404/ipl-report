e9975e3906465c3b72adbeb4cad7fd3d4c683746
sachchinannam <sachchinannam@gmail.com>
2026-05-25 18:28:03 +0000

    add

--- Pages/Winners.cshtml.cs
+++ Pages/Winners.cshtml.cs
@@ -13,22 +13,22 @@ namespace myapp.Pages
             // This is a temporary solution. The data should be read from the ipl.csv file.
             Winners = new List<Winner>
             {
-                new Winner { Year = 2008, Winner = "Rajasthan Royals", RunnerUp = "Chennai Super Kings" },
-                new Winner { Year = 2009, Winner = "Deccan Chargers", RunnerUp = "Royal Challengers Bangalore" },
-                new Winner { Year = 2010, Winner = "Chennai Super Kings", RunnerUp = "Mumbai Indians" },
-                new Winner { Year = 2011, Winner = "Chennai Super Kings", RunnerUp = "Royal Challengers Bangalore" },
-                new Winner { Year = 2012, Winner = "Kolkata Knight Riders", RunnerUp = "Chennai Super Kings" },
-                new Winner { Year = 2013, Winner = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
-                new Winner { Year = 2014, Winner = "Kolkata Knight Riders", RunnerUp = "Kings XI Punjab" },
-                new Winner { Year = 2015, Winner = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
-                new Winner { Year = 2016, Winner = "Sunrisers Hyderabad", RunnerUp = "Royal Challengers Bangalore" },
-                new Winner { Year = 2017, Winner = "Mumbai Indians", RunnerUp = "Rising Pune Supergiant" },
-                new Winner { Year = 2018, Winner = "Chennai Super Kings", RunnerUp = "Sunrisers Hyderabad" },
-                new Winner { Year = 2019, Winner = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
-                new Winner { Year = 2020, Winner = "Mumbai Indians", RunnerUp = "Delhi Capitals" },
-                new Winner { Year = 2021, Winner = "Chennai Super Kings", RunnerUp = "Kolkata Knight Riders" },
-                new Winner { Year = 2022, Winner = "Gujarat Titans", RunnerUp = "Rajasthan Royals" },
-                new Winner { Year = 2023, Winner = "Chennai Super Kings", RunnerUp = "Gujarat Titans" },
+                new Winner { Year = 2008, WinningTeam = "Rajasthan Royals", RunnerUp = "Chennai Super Kings" },
+                new Winner { Year = 2009, WinningTeam = "Deccan Chargers", RunnerUp = "Royal Challengers Bangalore" },
+                new Winner { Year = 2010, WinningTeam = "Chennai Super Kings", RunnerUp = "Mumbai Indians" },
+                new Winner { Year = 2011, WinningTeam = "Chennai Super Kings", RunnerUp = "Royal Challengers Bangalore" },
+                new Winner { Year = 2012, WinningTeam = "Kolkata Knight Riders", RunnerUp = "Chennai Super Kings" },
+                new Winner { Year = 2013, WinningTeam = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
+                new Winner { Year = 2014, WinningTeam = "Kolkata Knight Riders", RunnerUp = "Kings XI Punjab" },
+                new Winner { Year = 2015, WinningTeam = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
+                new Winner { Year = 2016, WinningTeam = "Sunrisers Hyderabad", RunnerUp = "Royal Challengers Bangalore" },
+                new Winner { Year = 2017, WinningTeam = "Mumbai Indians", RunnerUp = "Rising Pune Supergiant" },
+                new Winner { Year = 2018, WinningTeam = "Chennai Super Kings", RunnerUp = "Sunrisers Hyderabad" },
+                new Winner { Year = 2019, WinningTeam = "Mumbai Indians", RunnerUp = "Chennai Super Kings" },
+                new Winner { Year = 2020, WinningTeam = "Mumbai Indians", RunnerUp = "Delhi Capitals" },
+                new Winner { Year = 2021, WinningTeam = "Chennai Super Kings", RunnerUp = "Kolkata Knight Riders" },
+                new Winner { Year = 2022, WinningTeam = "Gujarat Titans", RunnerUp = "Rajasthan Royals" },
+                new Winner { Year = 2023, WinningTeam = "Chennai Super Kings", RunnerUp = "Gujarat Titans" },
             };
         }
     }
