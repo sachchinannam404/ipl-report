fe2acc3a94bfb29ebd037a1ea7ffa516cc479233
sachchinannam <sachchinannam@gmail.com>
2026-05-25 16:23:43 +0000

    add

---
+++ Data/SeedData.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.EntityFrameworkCore;
+using myapp.Models;
+
+namespace myapp.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any players.
+                if (context.Players.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Players.AddRange(
+                    new Player
+                    {
+                        Name = "Shohei Ohtani",
+                        Team = "Los Angeles Dodgers",
+                        Position = "Designated Hitter",
+                        Batting = "Left",
+                        Throwing = "Right"
+                    },
+                    new Player
+                    {
+                        Name = "Mookie Betts",
+                        Team = "Los Angeles Dodgers",
+                        Position = "Shortstop",
+                        Batting = "Right",
+                        Throwing = "Right"
+                    },
+                    new Player
+                    {
+                        Name = "Freddie Freeman",
+                        Team = "Los Angeles Dodgers",
+                        Position = "First Baseman",
+                        Batting = "Left",
+                        Throwing = "Right"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
