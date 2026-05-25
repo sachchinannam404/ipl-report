ad7c6f2c0da2e2065ce0c27023ca38be163ba8e8
sachchinannam <sachchinannam@gmail.com>
2026-05-25 18:44:28 +0000

    add

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,14 +13,10 @@ namespace myapp.Data
         public DbSet<Player> Players { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Coach> Coaches { get; set; }
+        public DbSet<IplData> IplData { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Team>()
-                .HasOne(t => t.Coach)
-                .WithOne(c => c.Team)
-                .HasForeignKey<Coach>(c => c.TeamId);
-
             modelBuilder.Entity<Team>()
                 .HasMany(t => t.Players)
                 .WithOne(p => p.Team)
