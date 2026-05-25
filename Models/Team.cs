9ae0fdeb76624773833cb859930a917803d79e78
sachchinannam <sachchinannam@gmail.com>
2026-05-25 17:40:35 +0000

    add

--- Models/Team.cs
+++ Models/Team.cs
@@ -1,11 +1,16 @@
+
+using System.Collections.Generic;
+
 namespace myapp.Models
 {
     public class Team
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Coach { get; set; }
+        public int CoachId { get; set; }
+        public Coach? Coach { get; set; }
         public string? City { get; set; }
         public string? HomeGround { get; set; }
+        public List<Player>? Players { get; set; }
     }
 }
