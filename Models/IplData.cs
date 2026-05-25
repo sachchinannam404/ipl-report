ee6cc29ce522ae7d65093d2580a6147891cc87ae
sachchinannam <sachchinannam@gmail.com>
2026-05-25 18:44:17 +0000

    add

--- Models/IplData.cs
+++ Models/IplData.cs
@@ -2,8 +2,15 @@ namespace myapp.Models
 {
     public class IplData
     {
-        public int Year { get; set; }
-        public string Winner { get; set; }
-        public string RunnerUp { get; set; }
+        public int Id { get; set; }
+        public int Season { get; set; }
+        public string? Team1 { get; set; }
+        public string? Team2 { get; set; }
+        public string? TossWinner { get; set; }
+        public string? TossDecision { get; set; }
+        public string? Winner { get; set; }
+        public string? PlayerOfMatch { get; set; }
+        public string? Venue { get; set; }
+        public string? City { get; set; }
     }
 }
