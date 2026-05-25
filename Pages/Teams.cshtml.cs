5cb27369b3b75a89754fd27dd03b0c70e9f392a7
sachchinannam <sachchinannam@gmail.com>
2026-05-25 16:10:33 +0000

    add add

--- Pages/Teams.cshtml.cs
+++ Pages/Teams.cshtml.cs
@@ -1,26 +1,22 @@
-
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using myapp.Data;
 using myapp.Models;
 
-namespace myapp.Pages
+namespace myapp.Pages;
+
+public class TeamsModel : PageModel
 {
-    public class TeamsModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public TeamsModel(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public TeamsModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IList<Team> Teams { get; set; } = new List<Team>();
 
-        public IList<Team> Teams { get; set; }
-
-        public void OnGet()
-        {
-            Teams = _context.Teams.ToList();
-        }
+    public void OnGet()
+    {
+        Teams = _context.Teams.ToList();
     }
 }
