7f244ea9a561ca07c8490d516f1670cad2f19e8a
sachchinannam <sachchinannam@gmail.com>
2026-05-25 18:15:59 +0000

    add

--- Models/Player.cs
+++ Models/Player.cs
@@ -5,10 +5,7 @@ namespace myapp.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public int TeamId { get; set; }
-        public Team? Team { get; set; }
-        public string? Position { get; set; }
-        public string? Batting { get; set; }
-        public string? Throwing { get; set; }
+        public string? Team { get; set; }
+        public string? Role { get; set; }
     }
 }
