e91e4f84a54f8cbfed9a2b4ae0e0b639027d4ee0
sachchinannam <sachchinannam@gmail.com>
2026-05-25 16:22:33 +0000

    add

--- obj/Debug/net9.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4ac0ada0daef48926107c2385c3be3ba31b0e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74d15cb9a48dab7f344607599affc0e933f79a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
