d8872fbea7024e99e81e05acb061cf9d6d1a59d3
sachchinannam <sachchinannam@gmail.com>
2026-05-25 16:10:18 +0000

    add

--- Pages/Players.cshtml.cs
+++ Pages/Players.cshtml.cs
@@ -1,26 +1,22 @@
-
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using myapp.Data;
 using myapp.Models;
 
-namespace myapp.Pages
+namespace myapp.Pages;
+
+public class PlayersModel : PageModel
 {
-    public class PlayersModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public PlayersModel(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public PlayersModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IList<Player> Players { get; set; } = new List<Player>();
 
-        public IList<Player> Players { get; set; }
-
-        public void OnGet()
-        {
-            Players = _context.Players.ToList();
-        }
+    public void OnGet()
+    {
+        Players = _context.Players.ToList();
     }
 }
